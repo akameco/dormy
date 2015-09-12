fea2c4bb584ed45eac18b148f39d03207e200b17
akameco <akameco.t@gmail.com>
2015-09-13 02:50:22 +0900

    Remove main.js

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import Promise from 'bluebird'
-import {Parse} from './parse.js'
-import {getAreaList} from './ryou.js'
-
-getAreaList().then((ryouList) => {
-  ryouList.forEach((x) => console.log(x.name, x.ryou))
-})
-
