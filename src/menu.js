5e11422d6d4f16be81c39e09d2147c842352a266
akameco <akameco.t@gmail.com>
2015-09-13 02:45:33 +0900

    :shusi: リファクタリング！！！

--- src/menu.js
+++ src/menu.js
@@ -1,22 +1,20 @@
 import Promise from 'bluebird'
-import request from 'request'
 import _ from 'lodash'
-import {toUTF8} from './util'
+import {post} from './util'
 import parse from './parse'
 
-function promisePostRequest(params) {
-  return new Promise((resolve, reject) => {
-    request.post(params, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        resolve(toUTF8(body))
-      } else {
-        reject(body)
-      }
-    })
+export function getMenus(ryou) {
+  let params = {
+    url: 'http://www.dnet.gr.jp/menulist/search/index.php',
+    form: {area: 'kanto', stuki: 'this_month', ryou: ryou},
+    encoding: null
+  }
+  return post(params).then((body) => {
+    return parse(body)
   })
 }
 
-export function getMenusList() {
+export default function getMenusList() {
   let ryous = ['a1', 'a2', 'b1', 'b2', 'hai']
   return Promise.all(ryous.map(getMenus))
     .then(list => {
@@ -26,17 +24,5 @@ export function getMenusList() {
       })
       return data
     })
-}
-
-export function getMenus(ryou) {
-  let params = {
-    url: 'http://www.dnet.gr.jp/menulist/search/index.php',
-    form: {area: 'kanto', stuki: 'this_month', ryou: ryou},
-    encoding: null
-  }
-
-  return promisePostRequest(params).then((body) => {
-    return parse(body)
-  })
 }
 
