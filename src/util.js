11d622c274153d19d5cbc7c4c36aef35154e72d5
akameco <akameco.t@gmail.com>
2015-09-13 02:45:02 +0900

    Add post and get func that using promise

--- src/util.js
+++ src/util.js
@@ -1,7 +1,31 @@
 import {Iconv} from 'iconv'
+import request from 'request'
 
 export function toUTF8(body) {
   let iconv = new Iconv('sjis', 'utf-8//TRANSLIT//IGNORE')
   return iconv.convert(body)
 }
 
+export function post(params) {
+  return new Promise((resolve, reject) => {
+    request.post(params, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        resolve(toUTF8(body))
+      } else {
+        reject(new Error(error))
+      }
+    })
+  })
+}
+
+export function get(params) {
+  return new Promise((resolve, reject) => {
+    request.get(params, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        resolve(toUTF8(body))
+      } else {
+        reject(new Error(error))
+      }
+    })
+  })
+}
